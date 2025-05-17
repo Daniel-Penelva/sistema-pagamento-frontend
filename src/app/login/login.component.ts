f4aa10338024770fa553c94dfbf8f353882cfe6a
Daniel Penelva <d4n.andrade@gmail.com>
2025-05-16 21:16:28 -0300

    feat: (funcionalidade: testando futuramente um serviço de autenticação de login e criando validação dos campos username e password) alterando loginComponent.html e loginComponent.ts
    A variável loginForm como FormGroup representa todo o formulário e seus controles.
    A injeção de FormBuilder permite criar o formulário de forma concisa e organizada.
    
    No método de inicialização ngOnInit foi criado validadores para exigir preenchimentos nos campos e-mail e password. Se inicia com valor inicial ' '.
    
    No método de login o uso do get com a notação loginForm.get('campo'), aqui acessa o controle específico do formulário e, com ?.value, extrai o valor. No HTML, o botão é desabilitado quando o formulário está inválido, de modo que esse método só será chamado quando todos os validadores estiverem satisfeitos.
    
    No template HTML o [formGroup]="loginForm" vincula o objeto loginForm do componente à estrutura HTML, permitindo o uso dos controles reativos dentro do mat-card.
     O <mat-error> mostra mensagens de erro quando o controle apresenta determinada falha.
    O [disabled]="loginForm.invalid" desabilita o botão enquanto o formulário não estiver válido, impedindo o envio com campos vazios ou inválidos.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+
+  public loginForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder){}
+
+  // Ao carregar o componente, inicializa o formulário com os campos username e password e defines os valores iniciais como vazios
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  login(): void {
+    let username = this.loginForm.get('username')?.value;
+    let password = this.loginForm.get('password')?.value;
+    console.log(username);
+    console.log(password);
+  }
 
 }
